322b2023ec80431417ebcef82c76330c11ef1b72
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-04 10:31:06 +0100

    solved task 4.2

--- task4/task4.py
+++ task4/task4.py
@@ -4,7 +4,10 @@ import re
 def main():
     file = open('input.txt', 'r')
     _sum = 0
+    scratchcards_count = 0
+    scratchcards_to_count = {}
 
+    card = 1
     while True:
         line = file.readline()
         if not line:
@@ -14,15 +17,29 @@ def main():
         my_numbers = set(map(int, re.findall('\d+', scratchcard[1])))
         winning_numbers = set(map(int, re.findall('\d+', scratchcard[0])))
 
-        count = 0
-        for num in my_numbers:
-            if num in winning_numbers:
-                count += 1
-
+        count = get_occurrences_number(winning_numbers, my_numbers)
         _sum += 2 ** (count - 1) if count > 0 else 0
 
+        # add original card
+        scratchcards_to_count[card] = scratchcards_to_count.get(card, 0) + 1
+        # add for count number of following cards the number of current card copies
+        for i in range(1, count + 1):
+            scratchcards_to_count[card + i] = scratchcards_to_count.get(card + i, 0) + scratchcards_to_count[card]
+        # increment card
+        scratchcards_count += scratchcards_to_count[card]
+        card += 1
+
     print("Points = " + str(_sum))
+    print("Total scratchcards do you end up with = " + str(scratchcards_count))
     file.close()
+
+
+def get_occurrences_number(original_set, my_numbers):
+    count = 0
+    for num in my_numbers:
+        if num in original_set:
+            count += 1
+    return count
 
 
 if __name__ == "__main__":
