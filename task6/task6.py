460048706c678b46bd7c124d7a50bc5931831bce
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-06 10:35:42 +0100

    solved task 6.2

--- task6/task6.py
+++ task6/task6.py
@@ -1,3 +1,6 @@
+import math
+
+
 def main():
     with open('input.txt', 'r') as file:
         lines = file.readlines()
@@ -8,18 +11,24 @@ def main():
 
         product = 1
         for time, distance in zip(time_list, distances):
-            product *= find_num_ways_to_beat_record(time, distance)
+            product *= get_ways_to_beat_record(time, distance)
 
         print("Task 1: product=" + str(product))
 
+        time_list = list(map(int, lines[0].split()[1:]))
+        distances = list(map(int, lines[1].split()[1:]))
+        product2 = 1
+        for time, distance in zip(time_list, distances):
+            product2 *= get_ways_to_beat_record(time, distance)
 
-def find_num_ways_to_beat_record(total_time, distance):
-    ways_to_win = 0
-    for hold_millis in range(total_time):
-        speed = hold_millis
-        if distance < speed * (total_time - hold_millis):
-            ways_to_win += 1
-    return ways_to_win
+        print("Task 2: product=" + str(product2))
+
+
+def get_ways_to_beat_record(total_time, distance):
+    upper_bound = math.floor((total_time + math.sqrt(total_time ** 2 - 4 * distance)) / 2)
+    lower_bound = math.ceil((total_time - math.sqrt(total_time ** 2 - 4 * distance)) / 2)
+
+    return upper_bound - lower_bound + 1
 
 
 if __name__ == "__main__":
