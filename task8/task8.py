649362ebb5d7e5433ea2597e0f55dc88d1d9e148
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-08 11:15:32 +0100

    started task 8.2

--- task8/task8.py
+++ task8/task8.py
@@ -14,6 +14,31 @@ def main():
             _map[row[0]] = (row[1], row[2])
 
         solve_a(_map, instructions)
+        solve_b(_map, instructions)
+
+
+def solve_b(_map, instructions):
+    positions = [position for position in _map.keys() if position.endswith("A")]
+    step = 0
+    while True:
+        index = step % (len(instructions) - 1)
+        step += 1
+        instruction = instructions[index]
+
+        all_z_positions = True
+        for i in range(len(positions)):
+            position = positions[i]
+            node = _map[position]
+            if instruction == "L":
+                positions[i] = node[0]
+            if instruction == "R":
+                positions[i] = node[1]
+            all_z_positions &= positions[i].endswith("Z")
+
+        if all_z_positions:
+            break
+
+    print("Task 2: How many steps does it take before you're only on nodes that end with Z" + str(step))
 
 
 # returns number of steps to go from AAA to ZZZ position
