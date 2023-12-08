e3c1a0ffd55a4b702964ab89e73935c349fcb044
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-08 11:44:48 +0100

    started task 8.2

--- task8/task8.py
+++ task8/task8.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -18,27 +19,33 @@ def main():
 
 
 def solve_b(_map, instructions):
-    positions = [position for position in _map.keys() if position.endswith("A")]
+    steps_to_reach_z = []
+    start_positions = [position for position in _map.keys() if position.endswith("A")]
     step = 0
     while True:
         index = step % (len(instructions) - 1)
         step += 1
         instruction = instructions[index]
 
-        all_z_positions = True
-        for i in range(len(positions)):
-            position = positions[i]
+        for i in range(len(start_positions)):
+            position = start_positions[i]
             node = _map[position]
             if instruction == "L":
-                positions[i] = node[0]
+                start_positions[i] = node[0]
             if instruction == "R":
-                positions[i] = node[1]
-            all_z_positions &= positions[i].endswith("Z")
+                start_positions[i] = node[1]
 
-        if all_z_positions:
+            if start_positions[i].endswith("Z"):
+                steps_to_reach_z.append(step)
+
+        if len(steps_to_reach_z) == len(start_positions):
             break
 
-    print("Task 2: How many steps does it take before you're only on nodes that end with Z" + str(step))
+    end_step = 1
+    # find the least common multiple which is the step where all path reach Z
+    for steps in steps_to_reach_z:
+        end_step = math.lcm(end_step, steps)
+    print("Task 2: How many steps does it take before you're only on nodes that end with Z? " + str(end_step))
 
 
 # returns number of steps to go from AAA to ZZZ position
