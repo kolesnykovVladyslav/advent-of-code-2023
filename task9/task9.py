8c27dfd45c6626b51453422b7d4274a41c773ba5
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-09 09:47:00 +0100

    solved task 9.2

--- task9/task9.py
+++ task9/task9.py
@@ -1,16 +1,32 @@
 def main():
     with open('input.txt', 'r') as file:
         lines = file.readlines()
+
+        sum = 0
+        for line in lines:
+            sum += solve_b(line)
+        print("Task 2: What is the sum of these extrapolated values? " + str(sum))
+
         sum = 0
         for line in lines:
             sum += solve_a(line)
         print("Task 1: What is the sum of these extrapolated values? " + str(sum))
 
 
-def solve_a(line):
+def solve_b(line):
+    histories = get_histories(line)
+
+    for i in range(len(histories) - 2, -1, -1):
+        value1 = histories[i][0]
+        value2 = histories[i + 1][0]
+        histories[i].insert(0, value1 - value2)
+
+    return histories[0][0]
+
+
+def get_histories(line):
     histories = []
     history = list(map(int, line.split()))
-
     while not (len(set(history)) == 1 and set(history).pop() == 0):
 
         diff_list = []
@@ -20,6 +36,11 @@ def solve_a(line):
             diff_list.append(value2 - value1)
         histories.append(history)
         history = diff_list
+    return histories
+
+
+def solve_a(line):
+    histories = get_histories(line)
 
     for i in range(len(histories) - 2, -1, -1):
         value1 = histories[i][-1]
