ec902cdc94b2358c1271d58ec65e361142d5cbbf
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-09 09:53:38 +0100

    task 9 refactoring

--- task9/task9.py
+++ task9/task9.py
@@ -1,34 +1,40 @@
 def main():
     with open('input.txt', 'r') as file:
         lines = file.readlines()
-
-        sum = 0
+        histories = []
         for line in lines:
-            sum += solve_b(line)
-        print("Task 2: What is the sum of these extrapolated values? " + str(sum))
+            histories.append(get_histories(line))
 
-        sum = 0
-        for line in lines:
-            sum += solve_a(line)
-        print("Task 1: What is the sum of these extrapolated values? " + str(sum))
+        print("Task 1: What is the sum of these extrapolated values? " + str(solve_a(histories)))
+        print("Task 2: What is the sum of these extrapolated values? " + str(solve_b(histories)))
 
 
-def solve_b(line):
-    histories = get_histories(line)
+def solve_b(all_histories):
+    sum = 0
+    for histories in all_histories:
+        for i in range(len(histories) - 2, -1, -1):
+            value1 = histories[i][0]
+            value2 = histories[i + 1][0]
+            histories[i].insert(0, value1 - value2)
+        sum += histories[0][0]
+    return sum
 
-    for i in range(len(histories) - 2, -1, -1):
-        value1 = histories[i][0]
-        value2 = histories[i + 1][0]
-        histories[i].insert(0, value1 - value2)
 
-    return histories[0][0]
+def solve_a(all_histories):
+    sum = 0
+    for histories in all_histories:
+        for i in range(len(histories) - 2, -1, -1):
+            value1 = histories[i][-1]
+            value2 = histories[i + 1][-1]
+            histories[i].append(value2 + value1)
+        sum += histories[0][-1]
+    return sum
 
 
 def get_histories(line):
     histories = []
     history = list(map(int, line.split()))
     while not (len(set(history)) == 1 and set(history).pop() == 0):
-
         diff_list = []
         for i in range(len(history) - 1):
             value1 = history[i]
@@ -37,17 +43,6 @@ def get_histories(line):
         histories.append(history)
         history = diff_list
     return histories
-
-
-def solve_a(line):
-    histories = get_histories(line)
-
-    for i in range(len(histories) - 2, -1, -1):
-        value1 = histories[i][-1]
-        value2 = histories[i + 1][-1]
-        histories[i].append(value2 + value1)
-
-    return histories[0][-1]
 
 
 if __name__ == "__main__":
