dd9051f31baa41a4320d69a6ce3238d53df3af58
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-13 14:54:41 +0100

    solved 13.1

--- task13/task13.py
+++ task13/task13.py
@@ -1,25 +1,32 @@
+def parse(lines):
+    grids = [[]]
+    for line in lines:
+        if line == '':
+            grids.append([])
+            continue
+
+        grids[-1].append(line)
+
+    return grids
+
+
 def main():
     with open("input.txt") as f:
         lines = [l for l in f.read().splitlines()]
+        grid = parse(lines)
+        solve_a(grid)
 
-        solve_a(lines)
 
-
-def solve_a(lines):
-    valley = []
+def solve_a(grid):
     sum = 0
-    for line in lines:
-        if len(line) > 0:
-            valley.append(line)
-        else:
-            sum += summarize_pattern(valley)
-            valley.clear()
+    for valley in grid:
+        sum += summarize_pattern(valley)
     print("What number do you get after summarizing all of your notes? " + str(sum))
 
 
 def summarize_pattern(valley):
     # horizontal reflection scan
-    sum = 100 * get_rows_number(valley)
+    sum = 100 * find_reflection(valley)
 
     # vertical reflection scan
     transponed_valley = ["" for _ in range(len(valley[0]))]
@@ -27,14 +34,14 @@ def summarize_pattern(valley):
         for j, char in enumerate(row):
             transponed_valley[j] += char
 
-    sum += get_rows_number(transponed_valley)
+    sum += find_reflection(transponed_valley)
 
     return sum
 
 
-def get_rows_number(_valley):
-    for i, line1 in enumerate(_valley[:-1]):
-        if line1 == _valley[i + 1]:
+def find_reflection(_valley):
+    for i, line in enumerate(_valley[:-1]):
+        if line == _valley[i + 1]:
             # found equal lines nearby
             above = _valley[i + 1:]
             below = _valley[:i + 1]
