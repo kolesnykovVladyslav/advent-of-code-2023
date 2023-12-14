f22249dca3e2bddd5865f5043a88558c05884fe6
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-14 18:35:27 +0100

    started 14.2

--- task14/task14.py
+++ task14/task14.py
@@ -1,23 +1,40 @@
 import re
+from functools import cache
+
+direction = 0
 
 
 def main():
     with open("input.txt") as f:
         lines = [l for l in f.read().splitlines()]
-        solve_a(lines)
+        solve_b(lines)
 
 
 def solve_a(grid):
-    t_grid = rotate_left(grid)
+    t_grid = rotate_left(tuple(grid))
     sum = count_load(t_grid)
     print("what is the total load on the north support beams? " + str(int(sum)))
 
 
+def solve_b(grid):
+    # 152173
+    cycles = 1000000000
+    steps = 1 + cycles * 4
+    t_grid = grid
+    for i in range(steps):
+        t_grid = rotate_left(tuple(t_grid))
+    sum = count_load(t_grid)
+    print("what is the total load on the north support beams? " + str(int(sum)))
+
+
+@cache
 def rotate_left(grid):
+    global direction
     grid = zip(*grid)
     new_grid = []
     for i, column in enumerate(grid):
         new_grid.append(shift_left(column))
+    direction += 1
     return new_grid
 
 
@@ -29,8 +46,11 @@ def shift_left(column):
         n = match.group().count("O")
         first_index = match.start()
         last_index = match.end()
-        new_column = new_column[:first_index] + "".join("O" * n) + "".join(
-            "." * (len(match.group()) - n)) + new_column[last_index:]
+
+        insert_str = "".join("O" * n) + "".join("." * (len(match.group()) - n))
+        if (direction % 4) > 1:
+            insert_str = insert_str[::-1]
+        new_column = new_column[:first_index] + insert_str + new_column[last_index:]
 
     return new_column
 
