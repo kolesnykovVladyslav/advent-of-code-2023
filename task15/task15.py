59463f1c5b2b21137b82746b3ef3992e75361cde
Q629717 <vladyslav.ka.kolesnykov@bmw.de>
2023-12-15 17:03:47 +0100

    solved 15.2

--- task15/task15.py
+++ task15/task15.py
@@ -5,7 +5,7 @@ direction = 0
 
 
 def main():
-    with open("test_input.txt") as f:
+    with open("input.txt") as f:
         line = [l for l in f.read().splitlines()][0]
         values = line.split(",")
         solve_b(values)
@@ -14,16 +14,26 @@ def main():
 
 def solve_b(values):
     _sum = 0
-    boxes = [OrderedDict() for _ in range(256)]
+    boxes = dict()
 
     for _str in values:
         label = re.search(r"[a-z]*", _str).group()
         box_number = get_hash(label)
+        if box_number not in boxes:
+            boxes[box_number] = OrderedDict()
+
         if "-" in _str and label in boxes[box_number]:
             boxes[box_number].pop(label)
+            if len(boxes[box_number]) == 0:
+                boxes.pop(box_number)
         elif "=" in _str:
-            focal_length = _str[-1]
+            focal_length = int(_str[-1])
             boxes[box_number][label] = focal_length
+
+    for box_num in boxes:
+        box = boxes[box_num]
+        for slot, label in enumerate(box):
+            _sum += (box_num + 1) * (slot + 1) * box[label]
 
     print("Part2: What is the sum of the results? " + str(int(_sum)))
 
