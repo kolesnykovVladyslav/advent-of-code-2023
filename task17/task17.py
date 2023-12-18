f9d33015d05afe84cb32ab68bb052af02f58aaf1
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2023-12-19 00:12:48 +0100

    started solving 17.1

--- task17/task17.py
+++ task17/task17.py
@@ -1,70 +1,14 @@
-import copy
-import enum
-
-
-class Direction(enum.Enum):
-    left = 0
-    up = 1
-    right = 2
-    down = 3
-
-
-class Crucible:
-    def __init__(self, coordinate, direction: Direction = None):
-        self.coordinate = coordinate
-        self.direction = direction
-        self.steps_forward = 0
-        self.path_length = 0
-
-    def turn_right_and_move(self):
-        self.steps_forward = 1
-        if self.direction == Direction.right:
-            self.direction = Direction.down
-            self.coordinate = (self.coordinate[0], self.coordinate[1] + 1)
-        elif self.direction == Direction.down:
-            self.direction = Direction.left
-            self.coordinate = (self.coordinate[0] - 1, self.coordinate[1])
-        elif self.direction == Direction.left:
-            self.direction = Direction.up
-            self.coordinate = (self.coordinate[0], self.coordinate[1] - 1)
-        elif self.direction == Direction.up:
-            self.direction = Direction.right
-            self.coordinate = (self.coordinate[0] + 1, self.coordinate[1])
-
-    def turn_left_and_move(self):
-        self.steps_forward = 1
-        if self.direction == Direction.down:
-            self.direction = Direction.right
-            self.coordinate = (self.coordinate[0] + 1, self.coordinate[1])
-        elif self.direction == Direction.right:
-            self.direction = Direction.up
-            self.coordinate = (self.coordinate[0], self.coordinate[1] - 1)
-        elif self.direction == Direction.up:
-            self.direction = Direction.left
-            self.coordinate = (self.coordinate[0] - 1, self.coordinate[1])
-        elif self.direction == Direction.left:
-            self.direction = Direction.down
-            self.coordinate = (self.coordinate[0], self.coordinate[1] + 1)
-
-    def move_forward(self):
-        self.steps_forward += 1
-        if self.steps_forward > 3:
-            return
-
-        if self.direction == Direction.down:
-            self.coordinate = (self.coordinate[0], self.coordinate[1] + 1)
-        elif self.direction == Direction.right:
-            self.coordinate = (self.coordinate[0] + 1, self.coordinate[1])
-        elif self.direction == Direction.up:
-            self.coordinate = (self.coordinate[0], self.coordinate[1] - 1)
-        elif self.direction == Direction.left:
-            self.coordinate = (self.coordinate[0] - 1, self.coordinate[1])
+import heapq
+import sys
+from collections import defaultdict
 
 
 class Node:
     def __init__(self, heat_loss: int):
         self.heat_loss = heat_loss
-        self.min_heat_loss_path = None
+        self.visited = False
+        self.dist = sys.maxsize
+        self.dir_counter = defaultdict(lambda: 0, {})
 
 
 def get_grid(lines):
@@ -80,38 +24,34 @@ def solve_a(lines):
     height = len(grid)
     width = len(grid[0])
 
-    crucible = Crucible((0, 0), Direction.right)
-    queue = [crucible]
+    grid[0][0].dist = 0
+    queue = [(grid[0][0].dist, 0, 0)]
 
     while len(queue) > 0:
-        crucible = queue.pop()
-        x, y = crucible.coordinate
-        if x < 0 or width - 1 < x or y < 0 or height - 1 < y:
+        _, x, y = heapq.heappop(queue)
+        node = grid[y][x]
+        if node.visited:
             continue
+        node.visited = True
+        # node.dir_counter.clear()
 
-        current_node = grid[y][x]
-        crucible.path_length += current_node.heat_loss
+        # travers neighbours
+        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+            new_x = x + dx
+            new_y = y + dy
+            if 0 <= new_x < width and 0 <= new_y < height:
+                neighbour = grid[new_y][new_x]
+                if node.dist + neighbour.heat_loss < neighbour.dist:
 
-        if grid[height - 1][width - 1].min_heat_loss_path is not None and grid[height - 1][
-            width - 1].min_heat_loss_path <= crucible.path_length + (width - x) + (height - y):
-            continue
+                    if node.dir_counter.get((dx, dy), 0) < 3:
+                        neighbour.dir_counter = dict(node.dir_counter)
+                        if (dx, dy) not in neighbour.dir_counter:
+                            neighbour.dir_counter.clear()
+                        neighbour.dir_counter[(dx, dy)] = neighbour.dir_counter.get((dx, dy), 0) + 1
+                        neighbour.dist = node.dist + neighbour.heat_loss
+                        heapq.heappush(queue, (neighbour.dist, new_x, new_y))
 
-        if current_node.min_heat_loss_path is not None and current_node.min_heat_loss_path <= crucible.path_length:
-            continue
-
-        current_node.min_heat_loss_path = crucible.path_length
-        # move
-        crucible_left = copy.copy(crucible)
-        crucible_left.turn_left_and_move()
-        crucible_right = copy.copy(crucible)
-        crucible_right.turn_right_and_move()
-        crucible_forward = copy.copy(crucible)
-        crucible_forward.move_forward()
-        queue.append(crucible_left)
-        queue.append(crucible_right)
-        queue.append(crucible_forward)
-
-    min_heat_loss_path = grid[height - 1][width - 1].min_heat_loss_path - grid[0][0].heat_loss
+    min_heat_loss_path = grid[height - 1][width - 1].dist - grid[0][0].heat_loss
     print("Part1: What is the least heat loss it can incur? " + str(min_heat_loss_path))
 
 
