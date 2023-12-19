8ba741dece36565b5066cd33d33d1990ee170598
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2023-12-19 17:30:38 +0100

    started 18.2

--- task18/task18.py
+++ task18/task18.py
@@ -1,20 +1,30 @@
+import enum
 import sys
+
+
+class Direction(enum.Enum):
+    R = 0
+    D = 1
+    L = 2
+    U = 3
 
 
 def main():
     with open("input.txt") as f:
         lines = [l for l in f.read().splitlines()]
-        solve_a(lines)
+        print("Part1: how many cubic meters of lava could it hold? " + str(get_cubic_meters(lines, get_instruction)))
+        print("Part2: how many cubic meters of lava could it hold? " + str(
+            get_cubic_meters(lines, get_hexadecimal_instruction)))
 
 
-def solve_a(lines):
+def get_cubic_meters(lines, convert_func):
     min_x, min_y = sys.maxsize, sys.maxsize
     max_x, max_y = -sys.maxsize, -sys.maxsize
     coordinate = (0, 0)
     path = set()
     path.add(coordinate)
     for line in lines:
-        direction, meters, rgb = line.split()
+        direction, meters = convert_func(line)
 
         dx, dy = 0, 0
         if direction == "R":
@@ -51,8 +61,19 @@ def solve_a(lines):
             if min_x - 1 <= next_coordinate[0] <= max_x + 1 and min_y - 1 <= next_coordinate[1] <= max_y + 1:
                 queue.append(next_coordinate)
 
-    cubic_meters = abs(max_x - min_x + 3) * abs(max_y - min_y + 3) - len(seen)
-    print("Part1: how many cubic meters of lava could it hold? " + str(cubic_meters))
+    return abs(max_x - min_x + 3) * abs(max_y - min_y + 3) - len(seen)
+
+
+def get_instruction(line):
+    direction, meters, _ = line.split()
+    return direction, meters
+
+
+def get_hexadecimal_instruction(line):
+    _, _, _hex = line.split()
+    meters = int(_hex[2:-2], 16)
+    direction = Direction(int(_hex[-2])).name
+    return direction, meters
 
 
 if __name__ == "__main__":
