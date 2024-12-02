36e182cba6639026feb15882dcfced4f09ca8be7
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-02 10:56:41 +0100

    solved task 2 part 2

--- 2024/task2/task2.py
+++ 2024/task2/task2.py
@@ -8,24 +8,49 @@ def main():
         # task 1
         calculate_safe_reports(reports)
         # task 2
+        calculate_safe_reports(reports, tolerate_count=1)
 
 
-def calculate_safe_reports(reports: list):
-    safe_reports_count = len([report for report in reports if is_safe_report(report)])
+def calculate_safe_reports(reports: list, tolerate_count: int = 0):
+    safe_reports_count = len([report for report in reports if is_safe_report(report, tolerate_count)])
     print("How many reports are safe? = " + str(safe_reports_count))
 
 
-def is_safe_report(report):
-    sign = report[0] - report[1]
+def is_safe_report(report, tolerate_count: int):
+    sign = np.sign(report[0] - report[1])
     for i, first in enumerate(report, start=1):
         if i >= len(report):
             break
 
         second = report[i]
         diff = first - second
-        if not (1 <= abs(diff) <= 3 and np.sign(sign) == np.sign(diff)):
-            return False
+        if not is_safe_diff(diff, sign):
+            if tolerate_count <= 0:
+                return False
+
+            if i == 2:
+                # maybe the problem is first element
+                new_report3 = remove_report(report, i - 2)
+                is_safe = is_safe_report(new_report3, tolerate_count - 1)
+                if is_safe:
+                    return True
+
+            new_report1 = remove_report(report, i)
+            new_report2 = remove_report(report, i - 1)
+
+            return is_safe_report(new_report1, tolerate_count - 1) or is_safe_report(new_report2, tolerate_count - 1)
+
     return True
+
+
+def is_safe_diff(diff, sign):
+    return 1 <= abs(diff) <= 3 and sign == np.sign(diff)
+
+
+def remove_report(report, index):
+    new_report = report.copy()
+    new_report.pop(index)
+    return new_report
 
 
 def get_reports(lines: list) -> list:
