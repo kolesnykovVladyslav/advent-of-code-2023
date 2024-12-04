eeea3b7ffb53cdc9d676b7b7e9a5154370f1fc95
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-04 08:59:29 +0100

    solved task 4 part 1

---
+++ 2024/task4/task4.py
@@ -0,0 +1,51 @@
+grid = []
+
+
+def main():
+    with open('input.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            grid.append(list(line))
+
+        # task 1
+        solve_part1()
+
+        # task 2
+        # solve(lines)
+
+
+def solve_part1():
+    xmas_count = 0
+    for i, row in enumerate(grid):
+        for j, char in enumerate(row):
+            if char == "X":
+                xmas_count += get_number_of_xmas_for_position(i, j, "MAS")
+            if char == "S":
+                xmas_count += get_number_of_xmas_for_position(i, j, "AMX")
+
+    print("How many times does XMAS appear? = " + str(xmas_count))
+
+
+def get_number_of_xmas_for_position(i, j, word) -> int:
+    up_right = True
+    right = True
+    low_right = True
+    down = True
+    for index, char in enumerate(word, 1):
+        right &= get_char_at_index(i, j + index) == char
+        up_right &= get_char_at_index(i - index, j + index) == char
+        low_right &= get_char_at_index(i + index, j + index) == char
+        down &= get_char_at_index(i + index, j) == char
+
+    return sum(bool(x) for x in [up_right, right, low_right, down])
+
+
+def get_char_at_index(i, j):
+    if not (0 <= i < len(grid)):
+        return ""
+    row = grid[i]
+    return row[j] if 0 <= j < len(row) else ""
+
+
+if __name__ == "__main__":
+    main()
