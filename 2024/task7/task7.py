babd1fd49a94420d4b8294c0c9cdce149444412d
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-07 09:09:03 +0100

    solving task 7 part 2

--- 2024/task7/task7.py
+++ 2024/task7/task7.py
@@ -7,28 +7,46 @@ def get_input(lines):
     return inputs
 
 
-def can_be_true(result_value, values):
-    intermediate_values = [result_value]
+def can_be_true_with_concat(result_value, values, with_concat=False):
+    first = values.pop(0)
+    intermediate_values = [first]
     for value in values:
         intermediate_values_for_value = intermediate_values
         intermediate_values = []
         for x in intermediate_values_for_value:
-            diff = x - value
-            if diff >= 0:
-                intermediate_values.append(diff)
-            if x % value == 0:
-                div = x / value
-                intermediate_values.append(div)
+            sum = x + value
+            if sum <= result_value:
+                intermediate_values.append(sum)
+            mult = x * value
+            if mult <= result_value:
+                intermediate_values.append(mult)
+            # concat
+            if not with_concat:
+                continue
+            concat = int(str(x) + str(value))
+            if concat <= result_value:
+                intermediate_values.append(concat)
 
-    return 0 in set(intermediate_values)
+    return result_value in set(intermediate_values)
 
 
 def solve1(inputs):
     result = 0
     for input in inputs:
         x = input[0]
-        values = reversed(input[1])
-        if can_be_true(x, values):
+        values = input[1].copy()
+        if can_be_true_with_concat(x, values, False):
+            result += x
+
+    print("total calibration result = " + str(result))
+
+
+def solve2(inputs):
+    result = 0
+    for input in inputs:
+        x = input[0]
+        values = input[1].copy()
+        if can_be_true_with_concat(x, values, True):
             result += x
 
     print("total calibration result = " + str(result))
@@ -41,6 +59,7 @@ def main():
         # task 1
         solve1(inputs)
         # task 2
+        solve2(inputs)
 
 
 if __name__ == "__main__":
