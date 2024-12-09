bf277211befa2081c18cbb2f47cd88aa3e98d56c
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-09 11:34:31 +0100

    solved task 9 part 2

--- 2024/task9/task9.py
+++ 2024/task9/task9.py
@@ -1,14 +1,17 @@
 class File:
-    def __init__(self, id, blocks, free_space):
+    def __init__(self, id, blocks, free_space, index=0):
         self.id = int(id)
         self.blocks = int(blocks)
         self.free_space = int(free_space)
+        self.index = int(index)
 
 
 def get_input(line):
     files = []
+    index = 0
     for i in range(0, len(line), 2):
-        files.append(File(id=i / 2, blocks=line[i], free_space=(line[i + 1] if i + 1 < len(line) else 0)))
+        files.append(File(id=i / 2, blocks=line[i], free_space=(line[i + 1] if i + 1 < len(line) else 0), index=index))
+        index += int(line[i]) + int(line[i + 1] if i + 1 < len(line) else 0)
     return files
 
 
@@ -19,7 +22,7 @@ def print_files(files):
     print(output)
 
 
-def solve(files):
+def solveA(files):
     file_system = []
     for i, file1 in enumerate(files):
         for _ in range(0, file1.blocks):
@@ -36,22 +39,48 @@ def solve(files):
         for _ in range(0, file1.free_space):
             file_system.append(0)
 
-    check_sum = 0
-    for i, id in enumerate(file_system):
-        check_sum += i * id
+    # print("".join([str(x) for x in file_system]))
+    check_sum = sum(i * id for i, id in enumerate(file_system))
+    print("checksum = " + str(check_sum))
+
+
+def solveB(files):
+    file_system = [0] * sum([file.blocks + file.free_space for file in files])
+    new_files = files.copy()
+    for i in range(len(files) - 1, 0, -1):
+        file2 = files[i]
+
+        for file1 in new_files:
+            if file1.index >= file2.index or file1 == file2:
+                continue
+            if file1.free_space >= file2.blocks:
+                file2.free_space = file1.free_space - file2.blocks
+                file1.free_space = 0
+                file2.index = file1.index + file1.blocks
+                new_files.remove(file2)
+                new_files.insert(new_files.index(file1) + 1, file2)
+                break
+
+    for file in new_files:
+        for k in range(file.blocks):
+            file_system[file.index + k] = file.id
+
+    # print("".join([str(x) for x in file_system]))
+    check_sum = sum(i * id for i, id in enumerate(file_system))
     print("checksum = " + str(check_sum))
 
 
 def main():
     with open('input.txt', 'r') as file:
         line = file.readlines()[0]
-        files = get_input(line)
-        print_files(files)
 
         # task 1
-        solve(files)
+        files = get_input(line)
+        solveA(files)
 
         # task 2
+        files = get_input(line)
+        solveB(files)
 
 
 if __name__ == "__main__":
