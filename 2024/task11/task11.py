f905deb1844330e8eb779bb21365e3e3cbfc0333
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-11 20:16:09 +0100

    solved task 11 part 2

--- 2024/task11/task11.py
+++ 2024/task11/task11.py
@@ -2,17 +2,12 @@ import time
 from functools import cache
 
 
-# cache = {}
-
-
 def get_input(lines):
     return [int(x) for x in lines[0].split()]
 
 
 @cache
 def blink(stone, times):
-    # if (stone, times) in cache:
-    #    return cache[(stone, times)]
     stones = []
     if stone == 0:
         stones = [1]
@@ -22,32 +17,29 @@ def blink(stone, times):
     else:
         stones = [stone * 2024]
 
-    # cache[(stone, times)] = stones
-
     if times == 1:
-        return stones
-    else:
-        result = []
-        for sub_stone in stones:
-            result += blink(sub_stone, times - 1)
+        return len(stones)
 
-        # cache[(stone, times)] = result
-        return result
+    count = 0
+    for sub_stone in stones:
+        count += blink(sub_stone, times - 1)
+
+    return count
 
 
 def solve(input, times):
-    stones = []
+    stones = 0
     for stone in input:
         stones += blink(stone, times)
-    print("Stones number: " + str(len(stones)))
+    print("Stones number: " + str(stones))
 
 
 def main():
     with open('input.txt', 'r') as file:
         lines = file.readlines()
         input = get_input(lines)
-        solve(input, 25)
-        # solve(input, 50)
+        # solve(input, 25)
+        solve(input, 75)
 
 
 if __name__ == "__main__":
