cdd328750ce8d2665f9c95473b0116a9eab58e06
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-16 19:55:01 +0100

    solving task 15 part 2

--- 2024/task15/task15.py
+++ 2024/task15/task15.py
@@ -1,7 +1,10 @@
 import time
 
+part_two = True
 walls = set()
 boxes = set()
+to_remove = set()
+to_add = set()
 
 
 def get_distance(pos):
@@ -23,25 +26,53 @@ def get_direction(char):
 
 
 def get_input(lines):
+    global _width
+    _width = len(lines[0]) - 1
+    if part_two:
+        _width *= 2
     initial_pos = None
     instructions = ""
     for y, line in enumerate(lines):
         if line.startswith(("<", "v", "^", ">")):
             instructions += line.rstrip()
             continue
+        global _height
+        _height = y
         for x, char in enumerate(line):
+            if part_two:
+                x *= 2
             if char == "\n":
                 continue
-            global _width, _height
-            _height = y + 1
-            _width = len(line) - 1
             if char == "@":
                 initial_pos = (x, y)
             if char == "#":
                 walls.add((x, y))
+                if part_two:
+                    walls.add((x + 1, y))
             if char == "O":
                 boxes.add((x, y))
     return instructions, initial_pos
+
+
+def print_map(pos):
+    global _width
+    global _height
+
+    for y in range(_height):
+        line = ""
+        for x in range(_width):
+            if pos == (x, y):
+                line += "@"
+            elif (x, y) in walls:
+                line += "#"
+            elif (x, y) in boxes:
+                line += "[]" if part_two else "0"
+            elif not part_two:
+                line += "."
+            elif (x - 1, y) not in boxes:
+                line += "."
+        print(line)
+    print()
 
 
 def can_move(x, y, instruction, is_start=True) -> bool:
@@ -61,40 +92,57 @@ def can_move(x, y, instruction, is_start=True) -> bool:
     return True
 
 
-def print_map(pos):
-    global _width
-    global _height
+def can_move_part2(x, y, instruction) -> bool:
+    dx, dy = get_direction(instruction)
+    pos = (x + dx, y + dy)
+    if pos in walls:
+        return False
+    if pos in boxes or (pos[0] - 1, pos[1]) in boxes:
+        if (pos[0] - 1, pos[1]) in boxes:
+            pos = (pos[0] - 1, pos[1])
+        next_x, next_y = pos
+        if instruction == ">":
+            next_x += 1
+        movable = can_move_part2(next_x, next_y, instruction)
+        if instruction in ["v", "^"]:
+            movable &= can_move_part2(next_x + 1, next_y, instruction)
+        if movable:
+            to_add.add((pos[0] + dx, pos[1] + dy))
+            to_remove.add(pos)
+        return movable
 
-    for y in range(_height):
-        line = ""
-        for x in range(_width):
-            if pos == (x, y):
-                line += "@"
-            elif (x, y) in walls:
-                line += "#"
-            elif (x, y) in boxes:
-                line += "0"
-            else:
-                line += "."
-        print(line)
-    print()
+    return True
 
 
 def solve1(lines):
+    global to_remove, to_add
     instructions, initial_pos = get_input(lines)
 
     x, y = initial_pos
+    print("Step: " + str(0))
+    print_map((x, y))
+
     for step, instruction in enumerate(instructions, start=1):
 
         dx, dy = get_direction(instruction)
         next_x = x + dx
         next_y = y + dy
 
-        if can_move(x, y, instruction):
-            x, y = next_x, next_y
+        if part_two:
+            if can_move_part2(x, y, instruction):
+                x, y = next_x, next_y
+                for pos in to_remove:
+                    boxes.remove(pos)
+                for pos in to_add:
+                    boxes.add(pos)
+            to_add.clear()
+            to_remove.clear()
+        else:
+            if can_move(x, y, instruction):
+                x, y = next_x, next_y
 
-        # print("Step: " + str(step))
-        # print_map((x, y))
+        print("Step: " + str(step) + " move:" + instruction)
+        print_map((x, y))
 
     result = sum([get_distance(pos) for pos in boxes])
     print(result)
