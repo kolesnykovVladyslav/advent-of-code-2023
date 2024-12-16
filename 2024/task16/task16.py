77d63f097bfa1179bca6de5921d3c3a4a62f0c3c
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-17 00:14:52 +0100

    solving task 16 part 2

--- 2024/task16/task16.py
+++ 2024/task16/task16.py
@@ -44,29 +44,52 @@ def get_input(lines):
 def solve1(lines):
     walls, initial_pos, finish_pos, width, height = get_input(lines)
 
-    visited_cells = defaultdict(lambda: sys.maxsize)
-    visited_cells[initial_pos] = 0
-    queue = [(initial_pos, Direction.right)]
+    score_of_cells = defaultdict(lambda: sys.maxsize)
+    visited_cells_path = defaultdict(lambda: set())
+    score_of_cells[(initial_pos, Direction.right)] = 0
+    score_of_cells[(initial_pos, Direction.up)] = 1000
+    queue = [(initial_pos, Direction.right), (initial_pos, Direction.up)]
 
     while len(queue) > 0:
-        pos, direction = queue.pop()
-        score = visited_cells.get(pos)
+        pos, direction = queue.pop(0)
+        score = score_of_cells[(pos, direction)]
+        visited_cells_path[(pos, direction)].add(pos)
+        if pos == finish_pos:
+            continue
+        new_pos = (pos[0] + direction.value[0], pos[1] + direction.value[1])
+        if new_pos in walls:
+            continue
 
         for dir, rotate_price in [(direction, 0),
                                   (turn_clockwise(direction), 1000),
-                                  (turn_clockwise(turn_clockwise(direction)), 2000),
                                   (turn_clockwise(turn_clockwise(turn_clockwise(direction))), 1000)]:
-            new_pos = (pos[0] + dir.value[0], pos[1] + dir.value[1])
-            if new_pos in walls:
-                continue
 
             new_score = score + rotate_price + 1
-            if visited_cells[new_pos] > new_score:
-                visited_cells[new_pos] = new_score
-                if new_pos != finish_pos:
-                    queue.append((new_pos, dir))
+            if score_of_cells[(new_pos, dir)] > new_score:
+                score_of_cells[(new_pos, dir)] = new_score
+                visited_cells_path[(new_pos, dir)] = visited_cells_path[(pos, direction)].copy()
+                queue.append((new_pos, dir))
+            elif score_of_cells[(new_pos, dir)] == new_score:
+                visited_cells_path[(new_pos, dir)].update(visited_cells_path[(pos, direction)])
+                queue.append((new_pos, dir))
 
-    print(visited_cells[finish_pos])
+    lowest_score = min(score_of_cells[finish_pos, Direction.up], score_of_cells[finish_pos, Direction.right])
+    print(lowest_score)
+    print(len(visited_cells_path[finish_pos, Direction.up]))
+    print_map(finish_pos, height, visited_cells_path, walls, width)
+
+
+def print_map(finish_pos, height, visited_cells_path, walls, width):
+    for y in range(height):
+        line = ""
+        for x in range(width):
+            if (x, y) in visited_cells_path[finish_pos, Direction.up]:
+                line += "0"
+            elif (x, y) in walls:
+                line += "#"
+            else:
+                line += "."
+        print(line)
 
 
 if __name__ == "__main__":
