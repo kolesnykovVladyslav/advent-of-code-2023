e0aceabfe896c8723009747985bd42fc7d0e70ba
Vladyslav Kolesnykov <vladyslav.ka.kolesnykov@bmw.de>
2024-12-19 12:20:31 +0100

    solved task 19 part 2

--- 2024/task19/task19.py
+++ 2024/task19/task19.py
@@ -1,30 +1,31 @@
 import time
 
-cache = {}
 
-
-def can_be_made(input, patterns, max_length):
+def count_ways(input, patterns, max_length, cache={}):
     if input in cache:
         return cache[input]
     if input == "":
-        return True
+        return 1
+    total_ways = 0
     for i in range(1, min(max_length, len(input)) + 1):
         part = input[:i]
-        if part in patterns and can_be_made(input[i:], patterns, max_length):
-            cache[input] = True
-            return True
-    cache[input] = False
-    return False
+        if part in patterns:
+            total_ways += count_ways(input[i:], patterns, max_length, cache)
+    cache[input] = total_ways
+    return total_ways
 
 
 def solve(lines):
     patterns = set(lines[0].strip().replace(" ", "").split(','))
     max_length = max([len(pattern) for pattern in patterns])
     counter = 0
+    total_ways = 0
     for input in lines[2:]:
-        if can_be_made(input.strip(), patterns, max_length):
-            counter += 1
+        ways = count_ways(input.strip(), patterns, max_length)
+        counter += int(ways > 0)
+        total_ways += ways
     print(counter)
+    print(total_ways)
 
 
 if __name__ == "__main__":
