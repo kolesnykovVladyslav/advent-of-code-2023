f690970e09e0f6733422ee0598d7683c9d27fa5b
Vladyslav Kolesnykov <vlad.kolesnykov@gmail.com>
2024-12-23 21:27:46 +0100

    solved task23.2

--- 2024/task23/task23.py
+++ 2024/task23/task23.py
@@ -35,9 +35,41 @@ def solve(lines):
     print(len(triangles))
 
 
+def bron_kerbosch(R, P, X, graph, cliques):
+    if not P and not X:
+        cliques.append(R)
+        return
+    for v in list(P):
+        bron_kerbosch(R.union([v]), P.intersection(graph[v]), X.intersection(graph[v]), graph, cliques)
+        P.remove(v)
+        X.add(v)
+
+
+def find_largest_clique(graph):
+    cliques = []
+    bron_kerbosch(set(), set(graph.keys()), set(), graph, cliques)
+    largest_clique = max(cliques, key=len)
+    return largest_clique
+
+
+def generate_password(clique):
+    sorted_clique = sorted(clique)
+    password = ",".join(sorted_clique)
+    return password
+
+
+def solve2(lines):
+    connections = parse_input(lines)
+    graph = build_graph(connections)
+    largest_clique = find_largest_clique(graph)
+    password = generate_password(largest_clique)
+    print(password)
+
+
 if __name__ == "__main__":
     start_time = time.time()
     with open('input.txt', 'r') as file:
         lines = file.readlines()
         solve(lines)
+        solve2(lines)
     print("--- %s seconds ---" % (time.time() - start_time))
